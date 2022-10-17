6ace4f5d7acef8e321cb50fbf5660f23e099f648
kerendre <108333919+kerendre@users.noreply.github.com>
2022-10-17 17:04:06 +0300

    Update mail_brain.py

--- mail_brain.py
+++ mail_brain.py
@@ -1,14 +1,11 @@
 import smtplib
-import os
-
+import personal_setting
 
 def send_mail(to_address ,mail_head, mail_body):
-    ''' sending mail with smtp, in order to use gmail, you may need to create and use app passwords and use  2-Step-Verification
-     follow the instraction: https://support.google.com/mail/answer/185833?hl=en-GB'''
-    my_gmail = os.environ.get("MY_GMAIL_APP_ACCOUNT")
-    my_gmail_app_password = os.environ.get("MY_GMAIL_APP_PASSWORD")
 
-    # Sending Email with Python
+    my_gmail=personal_setting.my_gmail
+    my_gmail_app_password=personal_setting.my_gmail_app_password
+    # Sending Email with SMTP
     with smtplib.SMTP("smtp.gmail.com") as connection:
         connection.starttls()
         connection.login(user=my_gmail, password=my_gmail_app_password)
